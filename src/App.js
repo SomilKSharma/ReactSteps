b0487afac5b4154f3b09c3a5c3326cf53bda9ead
Somil Sharma <somil81@icloud.com>
2023-08-30 23:29:57 +0530

    Updates on the JSX.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+// Array
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  // variable for steps
+  const steps = 2
+  // return the jsx
   return (
-    <div>
-      Hello React
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${steps === 1 ? "active" : ""}`}>
+          1
+        </div>
+        <div className={`${steps === 2 ? "active" : ""}`}>
+          2
+        </div>
+        <div className={`${steps === 3 ? "active" : ""}`}>
+          3
+        </div>
+      </div>
+
+      <p className="message">
+        Step {steps}: {messages[steps - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() => alert('Previous')}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() => alert('Next')}
+          onMouseEnter={() => alert('Mouse hover')}
+        >
+          Next
+        </button>
+      </div>
     </div>
   )
 }
